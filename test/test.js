cc1c3a3f1b3d4b1fdabf6e70f90289bc8b19cfc3
Erik <erikdesjardins@users.noreply.github.com>
2018-03-03 19:46:05 -0500

    make tests work crossplatform

--- test/test.js
+++ test/test.js
@@ -37,15 +37,29 @@ test('basic usage', async t => {
 	const subchild = readFileSync(join(__dirname, 'dist/children/subchild/subchild.js'), 'utf8');
 	const defaults = readFileSync(join(__dirname, 'dist/defaults.js'), 'utf8');
 	const manifest = readFileSync(join(__dirname, 'dist/inertOut/manifest.notjson'), 'utf8');
-	const defaultsWithHash = readFileSync(join(__dirname, 'dist/defaults.70b6b5.js'), 'utf8');
-	const defaultsInertWithHash = readFileSync(join(__dirname, 'dist/defaults-inert.3876bd.js'), 'utf8');
+	const defaultsWithHash = readFileSync(
+		join(__dirname, process.platform === 'win32' ? 'dist/defaults.70b6b5.js' : 'dist/defaults.9eda03.js'),
+		'utf8'
+	);
+	const defaultsInertWithHash = readFileSync(
+		join(__dirname, process.platform === 'win32' ? 'dist/defaults-inert.3876bd.js' : 'dist/defaults-inert.2f0c2f.js'),
+		'utf8'
+	);
 
 	t.regex(mainBundle, /"other\.entry\.spawned\.js"/, 'references spawned other entry');
 	t.regex(mainBundle, /"children(\/|\\\\)child1\.js"/, 'references spawned child1');
 	t.regex(mainBundle, /"defaults\.js"/, 'references spawned defaults');
 	t.regex(mainBundle, /"inertOut(\/|\\\\)manifest\.notjson"/, 'references spawned manifest.json');
-	t.regex(mainBundle, /"defaults.70b6b5.js"/, 'references spawned defaults with hash');
-	t.regex(mainBundle, /"defaults-inert.3876bd.js"/, 'references spawned inert defaults with hash');
+	t.regex(
+		mainBundle,
+		process.platform === 'win32' ? /"defaults.70b6b5.js"/ : /"defaults.9eda03.js"/,
+		'references spawned defaults with hash'
+	);
+	t.regex(
+		mainBundle,
+		process.platform === 'win32' ? /"defaults-inert.3876bd.js"/ : /"defaults-inert.2f0c2f.js"/,
+		'references spawned inert defaults with hash'
+	);
 	t.regex(mainBundle, /__webpack_require__\.p = ""/, 'publicPath is empty');
 
 	t.regex(otherEntry, /\bfunction __webpack_require__\b/, 'has prelude');
